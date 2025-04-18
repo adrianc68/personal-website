86b3710b2bdeab02b442de1026877e50cec2c77a
adrianc68 <angeladriancamalgarcia@hotmail.com>
2025-04-18 16:14:19 -0600

    feat: update footer and nav-bar

--- src/components/nav-bar/nav-bar.component.ts
+++ src/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   standalone: true,
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 
 }
