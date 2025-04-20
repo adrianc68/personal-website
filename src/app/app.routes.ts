51786765f6f67724f5f3fadad6a6bcabc77bb20f
adrianc68 <angeladriancamalgarcia@hotmail.com>
2025-04-19 18:07:51 -0600

    feat: projects and not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from '../pages/landing-page/landing-page.component';
+import { ProjectsPageComponent } from '../pages/projects-page/projects-page.component';
+import { ContactPageComponent } from '../pages/contact-page/contact-page.component';
+import { NotFoundPageComponent } from '../pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
-  { path: "", component: LandingPageComponent, title: "Angel Adrian - Inicio" }
+  { path: "", component: LandingPageComponent, title: "Home" },
+  { path: "projects", component: ProjectsPageComponent, title: "Projects" },
+  { path: "contact", component: ContactPageComponent, title: "Contact" },
+  { path: "**", component: NotFoundPageComponent, title: "Not found" }
 
 ];
---
+++ src/pages/not-found-page/not-found-page.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found-page',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './not-found-page.component.html',
+  styleUrl: './not-found-page.component.scss'
+})
+export class NotFoundPageComponent {
+
+}
